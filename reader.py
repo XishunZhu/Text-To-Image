8b92731614c4bc03d6fe0ba326e92a99ecb90cb5
Landon Smith <landjbs@gmail.com>
2019-10-17 15:35:46 -0400

    data gitignore

--- reader.py
+++ reader.py
@@ -1,3 +1,11 @@
 """
 All functions for analysis and processing of dataset
 """
+
+import time as t
+import os
+
+with open('data/inData/Train-GCC-training.tsv') as imgFile:
+    for line in imgFile:
+        print(line, end=f'{"-"*80}\n\n\n')
+        t.sleep(1)
