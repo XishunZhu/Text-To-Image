fcf37a5a66a275ae637ff7a3df74faaeb3823204
Landon Smith <landjbs@gmail.com>
2019-10-17 15:46:25 -0400

    max_len type assert

--- model.py
+++ model.py
@@ -2,6 +2,9 @@
 Model architecture
 """
 
+import utils as u
+
 class Text_GAN(object):
     def __init__(self, max_text_len):
+        u.type_assert('max_text_len', max_text_len, int)
         self.max_text_len = max_text_len
